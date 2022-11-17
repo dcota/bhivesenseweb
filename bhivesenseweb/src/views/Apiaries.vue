fe9e78c567fe82a70bea9af998ca95bb585af7c0
dcota <duartenunocota@gmail.com>
2022-11-17 18:26:00 -0100

    sidebar

---
+++ bhivesenseweb/src/views/Apiaries.vue
@@ -0,0 +1,108 @@
+<template>
+      <div class="container justify-content-center">
+        <div class="row mt-5">
+          <div class="col-12 col-md-6 col-lg-3">
+            <div class="card mb-5 mh-100" style="border-radius: 35px;">
+              <img src="../assets/IMG1220.png" class="card-img-top" alt="..."
+                style="width: 50%;display: block;margin-right: auto;margin-left: auto;">
+              <hr>
+              <div class="card-body">
+                <h5 class="card-title text-center">Apiary</h5>
+                <p class="card-text text-center">Furnas</p>
+                <div class="text-center">
+                  <a href="#" class="btn text-center bn "><strong>Get
+                      inside</strong></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-6 col-lg-3">
+            <div class="card mb-5 mh-100" style="border-radius: 35px;">
+              <img src="../assets/IMG1220.png" class="card-img-top" alt="..."
+                style="width: 50%;display: block;margin-right: auto;margin-left: auto;">
+              <hr>
+              <div class="card-body">
+                <h5 class="card-title text-center">Apiary</h5>
+                <p class="card-text text-center">Furnas</p>
+                <div class="text-center">
+                  <a href="#" class="btn text-center bn "><strong>Get
+                      inside</strong></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+            <div class="card mb-5 mh-100" style="border-radius: 35px;">
+              <img src="../assets/IMG1220.png" class="card-img-top" alt="..."
+                style="width: 50%;display: block;margin-right: auto;margin-left: auto;">
+              <hr>
+              <div class="card-body">
+                <h5 class="card-title text-center">Apiary</h5>
+                <p class="card-text text-center">Furnas</p>
+                <div class="text-center">
+                  <a href="#" class="btn text-center bn "><strong>Get
+                      inside</strong></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+            <div class="card mb-5 mh-100" style="border-radius: 35px;">
+              <img src="../assets/IMG1220.png" class="card-img-top" alt="..."
+                style="width: 50%;display: block;margin-right: auto;margin-left: auto;">
+              <hr>
+              <div class="card-body">
+                <h5 class="card-title text-center">Apiary</h5>
+                <p class="card-text text-center">Furnas</p>
+                <div class="text-center">
+                  <a href="#" class="btn text-center bn"><strong>Get
+                      inside</strong></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          --
+        </div>
+
+
+    </div>
+    <!--<div class="row text-center" style="position: fixed; bottom:0px">
+        <div class="col-12 social-media footer">
+          <ul>
+          <li>
+            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+          </li>
+          <li>
+            <a href="#"
+              ><i class="fa fa-linkedin-square" aria-hidden="true"></i
+            ></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+          </li>
+        </ul>
+        <p><strong>© Copyright 2022 BHiveSense - todos os direitos reservados</strong></p>
+        </div>
+</div>-->
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
---
+++ bhivesenseweb/src/views/Dashboard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="dashboard">
+        <Sidebar/>
+        <div class="content text-center">
+            <router-view/>
+        </div>
+    </div>
+</template>
+
+<script>
+import Sidebar from '../components/Sidebar'
+export default {
+    name: 'home',
+    components: {
+        Sidebar
+    }
+}
+</script>
+
+<style>
+.dashboard {
+    display: grid;
+    grid-template-columns: 300px auto;
+    /*height: 100vh;
+    width: 100vw;*/
+}
+
+.content {
+    background-color: #ebc002;
+}
+</style>
